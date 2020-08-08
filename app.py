235cca2e66b205bb8708917e00ebc4acf4b413e4
comeuser85924 <zx2515296964@gmail.com>
2020-08-08 13:53:28 +0800

    First edition

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 
-from flask import Flask, request, abort
+from flask import Flask, request, abort, render_template
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import *
 import configparser
 import requests
-
+import json
 import unicodedata # 幫助我們全形轉半行
 app = Flask(__name__)
 
@@ -16,17 +16,19 @@ config.read('config.ini')
 line_bot_api = LineBotApi(config.get('line-bot', 'channel_access_token'))
 handler = WebhookHandler(config.get('line-bot', 'channel_secret'))
 
+@app.route("/")
+def home():
+    return render_template("home.html")
+
 @app.route("/callback", methods=['POST'])
 def callback():
     signature = request.headers['X-Line-Signature']
-
+    
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
-    print(body, signature)
     try:
-        # print(body, signature)
+        print('確認進到callback')
         handler.handle(body, signature)
-
     except InvalidSignatureError:
         abort(400)
 
@@ -37,16 +39,21 @@ def callback():
 def handle_message(event):
     
     msg = event.message.text
-    print(msg)
     # print(type(msg))
     msg = msg.encode('utf-8')
     if ('帳號' in event.message.text):
+        
         msg = unicodedata.normalize('NFKC',event.message.text)
         account = msg.split(':')[1]
-        url = 'https://www.instagram.com/'+account+'/?__a=1'
-        response = requests.get(url)
+        url = "https://www.instagram.com/"+account+"/"
+        querystring = {"__a":"1"}
+        payload = ""
+        headers = {'cookie': 'ig_did=AA7BB833-E8ED-4B57-94A7-6F54995F6613; mid=Xy5jkQALAAENDAoMv4SJhALi6s0C; fbm_124024574287414=base_domain=.instagram.com; fbsr_124024574287414=Pik34X2Ds4COWu9V7Z9Z2gKw9ieLQmjFII3NBkf75_g.eyJ1c2VyX2lkIjoiMTAwMDAwMzM1ODgxMDM2IiwiY29kZSI6IkFRQm5WaUJFTmQ3QTNFQW5WS0ZPUnZ2aGd2ZzV3MXNBdWd2RWxrTXJjMWZNbVNmOEZKcE5oM1ViQmVBdkZucktJR3lMOWt0eXJPTVgyWWlJSG4wVnlwT0RmZnRWNDA4QmRaVGJfWXVjX1ZDXzl2dTZEWnN4bDF1bmxYZ1pEbnUycjd5NHBqX1djYmc4OVNfYzJDNmpwdkNybVNBcmlQY3N4ZFhVTW1oaldMUVk5TU5KRHUxOFlDRnpFODlLWWlyYjZmTzk1TnBmNVcxX3dJRDREbm5BQnhjdFRpVk5jaGMxMjNQUGQwLWxCRTM5b2ZsVFB6ZlJZZGpubElFc1FZVkxrNk1ZREppejdwNmR4U1dxZFJEMDhfbnZtZ2hoaWpxS3ZWeW5BcUZJcFhvUlV6M3NVNjROZ0tTQ1RISUtfTURRRWdxdXh6NUJweDVhZU1XZ3NBWXdzbGpOIiwib2F1dGhfdG9rZW4iOiJFQUFCd3pMaXhuallCQUxrZ2xwQWJsWkNFdGlMUlRqS24zUTNMQzM4Zkw3dGRaQWtGbWk2V0JaQkxEQ1Jxb2hwNGFsQ3hGOXhBbEo3UmNkRzRzR0dueGRqWVpBMmxaQjlsVFpBbGdSRnJxWWJua1RTWkJqQmZOU3Y5cVRuMzNJN1lTZ280bGpFZVh5NTY3c1E4WkJDRmhRb3FwN2t1eWNMNlpBOUhLMWJnbFZUWkJHYmRET0hrMWFEOXZnIiwiYWxnb3JpdGhtIjoiSE1BQy1TSEEyNTYiLCJpc3N1ZWRfYXQiOjE1OTY4NzU2ODJ9; csrftoken=fxjaPYfL12MdeHJE0Hxp97sqH2InqCwa; ds_user_id=1698926261; sessionid=1698926261%3AdsGUDhGrIRqHOl%3A20; shbid=15998; shbts=1596875684.2646637; rur=VLL; urlgen="{\"150.116.182.98\": 131627}:1k4LU8:WLU9Hz1_Sd7u40hAAixjKqQRxmM"'}
+        response = requests.request("GET", url, data=payload, headers=headers, params=querystring)
+
         if(response.status_code == 200):
             personalFile = response.json()['graphql']['user']['edge_owner_to_timeline_media']['edges']
+           
             array = []
             for idx in range(len(personalFile)):
                 array.append({
@@ -72,6 +79,7 @@ def handle_message(event):
                             "paddingAll": "0px"
                         }
                     })
+            # print(array[0:10])
             flex_message = FlexSendMessage(
                 alt_text='潘多拉之盒已開啟',
                 contents={
@@ -79,6 +87,7 @@ def handle_message(event):
                     "contents": array[0:10]
                 }
             )
+            # print(flex_message)
             line_bot_api.reply_message(event.reply_token, flex_message)
         else:
             line_bot_api.reply_message(
@@ -86,9 +95,12 @@ def handle_message(event):
     elif ('顯示圖檔' in event.message.text):
         msg = unicodedata.normalize('NFKC',event.message.text)
         account = msg.split(':')[1].split('-')[0]
-        ig_url = 'https://www.instagram.com/'+account+'/?__a=1'
         index = msg.split(':')[1].split('-')[1]
-        response = requests.get(ig_url)
+        url = "https://www.instagram.com/"+account+"/"
+        querystring = {"__a":"1"}
+        payload = ""
+        headers = {'cookie': 'ig_did=AA7BB833-E8ED-4B57-94A7-6F54995F6613; mid=Xy5jkQALAAENDAoMv4SJhALi6s0C; fbm_124024574287414=base_domain=.instagram.com; fbsr_124024574287414=Pik34X2Ds4COWu9V7Z9Z2gKw9ieLQmjFII3NBkf75_g.eyJ1c2VyX2lkIjoiMTAwMDAwMzM1ODgxMDM2IiwiY29kZSI6IkFRQm5WaUJFTmQ3QTNFQW5WS0ZPUnZ2aGd2ZzV3MXNBdWd2RWxrTXJjMWZNbVNmOEZKcE5oM1ViQmVBdkZucktJR3lMOWt0eXJPTVgyWWlJSG4wVnlwT0RmZnRWNDA4QmRaVGJfWXVjX1ZDXzl2dTZEWnN4bDF1bmxYZ1pEbnUycjd5NHBqX1djYmc4OVNfYzJDNmpwdkNybVNBcmlQY3N4ZFhVTW1oaldMUVk5TU5KRHUxOFlDRnpFODlLWWlyYjZmTzk1TnBmNVcxX3dJRDREbm5BQnhjdFRpVk5jaGMxMjNQUGQwLWxCRTM5b2ZsVFB6ZlJZZGpubElFc1FZVkxrNk1ZREppejdwNmR4U1dxZFJEMDhfbnZtZ2hoaWpxS3ZWeW5BcUZJcFhvUlV6M3NVNjROZ0tTQ1RISUtfTURRRWdxdXh6NUJweDVhZU1XZ3NBWXdzbGpOIiwib2F1dGhfdG9rZW4iOiJFQUFCd3pMaXhuallCQUxrZ2xwQWJsWkNFdGlMUlRqS24zUTNMQzM4Zkw3dGRaQWtGbWk2V0JaQkxEQ1Jxb2hwNGFsQ3hGOXhBbEo3UmNkRzRzR0dueGRqWVpBMmxaQjlsVFpBbGdSRnJxWWJua1RTWkJqQmZOU3Y5cVRuMzNJN1lTZ280bGpFZVh5NTY3c1E4WkJDRmhRb3FwN2t1eWNMNlpBOUhLMWJnbFZUWkJHYmRET0hrMWFEOXZnIiwiYWxnb3JpdGhtIjoiSE1BQy1TSEEyNTYiLCJpc3N1ZWRfYXQiOjE1OTY4NzU2ODJ9; csrftoken=fxjaPYfL12MdeHJE0Hxp97sqH2InqCwa; ds_user_id=1698926261; sessionid=1698926261%3AdsGUDhGrIRqHOl%3A20; shbid=15998; shbts=1596875684.2646637; rur=VLL; urlgen="{\"150.116.182.98\": 131627}:1k4LU8:WLU9Hz1_Sd7u40hAAixjKqQRxmM"'}
+        response = requests.request("GET", url, data=payload, headers=headers, params=querystring)
         if(response.status_code == 200):
             personalFile = response.json()['graphql']['user']['edge_owner_to_timeline_media']['edges']
             line_bot_api.reply_message(event.reply_token, ImageSendMessage(
